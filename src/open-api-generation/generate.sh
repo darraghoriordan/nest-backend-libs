4d22f8706131cac569df6352f1f2d145c8fdae3f
Darragh ORiordan <darragh.oriordan@gmail.com>
2021-11-28 17:14:54 +1100

    fix: paths in generate script

--- src/open-api-generation/generate.sh
+++ src/open-api-generation/generate.sh
@@ -2,8 +2,8 @@
 rm -rf ../shared-api-client/src
 rm -rf ../shared-api-client/dist
 npx @openapitools/openapi-generator-cli generate -i ./open-api/swagger.json -g typescript-fetch -o ../shared-api-client --additional-properties=typescriptThreePlus=true,withInterfaces=true,npmName=shared-api-client,supportsES6=true,modelPropertyNaming=original,enumPropertyNaming=original
-cp ./api-client-templates/api-client-ts-config-template.json ../shared-api-client/tsconfig.json
-cp ./api-client-templates/package-json-template.json ../shared-api-client/package.json
+cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/api-client-ts-config-template.json ../shared-api-client/tsconfig.json
+cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/package-json-template.json ../shared-api-client/package.json
 cd ../shared-api-client
 yarn
 
