5f3417164191401d3d95dafb238e46e4be88c706
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-12-02 22:25:10 +1100

    feat: open-api generation custom locations

--- src/open-api-generation/generate.sh
+++ src/open-api-generation/generate.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-rm -rf ../shared-api-client/src
-rm -rf ../shared-api-client/dist
-npx @openapitools/openapi-generator-cli generate -i ./open-api/swagger.json -g typescript-fetch -o ../shared-api-client --additional-properties=typescriptThreePlus=true,withInterfaces=true,npmName=shared-api-client,supportsES6=true,modelPropertyNaming=original,enumPropertyNaming=original
-cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/api-client-ts-config-template.json ../shared-api-client/tsconfig.json
-cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/package-json-template.json ../shared-api-client/package.json
-cd ../shared-api-client
+rm -rf $1/src
+rm -rf $1/dist
+npx @openapitools/openapi-generator-cli generate -i ./open-api/swagger.json -g typescript-fetch -o $1 --additional-properties=typescriptThreePlus=true,withInterfaces=true,npmName=shared-api-client,supportsES6=true,modelPropertyNaming=original,enumPropertyNaming=original
+cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/api-client-ts-config-template.json $1/tsconfig.json
+cp ./node_modules/@darraghor/nest-backend-libs/dist/open-api-generation/api-client-templates/package-json-template.json $1/package.json
+cd $1
 yarn
 
 # install latest into the client
-cd ../frontend
+cd $2
 rm -rf ./node_modules/shared-api-client
 yarn install --check-files
 
 # install latest into the e2e tests
-cd ../e2e-backend
+cd $3
 rm -rf ./node_modules/shared-api-client
 yarn install --check-files
