8775d0c78bb3b00742139ce52aa8f7cf206c3c19
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-01-28 14:05:31 +1100

    feat: add cli

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import "reflect-metadata";
 import yargs from "yargs";
+import {WriteApiClientCommand} from "./commands/WriteApiClientCommand";
 import {WriteTypeOrmConfigCommand} from "./commands/WriteTypeOrmConfigCommand";
 
 void yargs
     .usage("Usage: $0 <command> [options]")
     .command(new WriteTypeOrmConfigCommand())
+    .command(new WriteApiClientCommand())
     .recommendCommands()
     .demandCommand(1)
     .strict()
