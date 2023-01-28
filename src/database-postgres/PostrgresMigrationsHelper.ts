96f0114ac42186ef1615662d360460f313da886d
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-01-28 17:08:42 +1100

    fix: cli

--- src/database-postgres/PostrgresMigrationsHelper.ts
+++ src/database-postgres/PostrgresMigrationsHelper.ts
@@ -1,6 +1,7 @@
 import {DataSource} from "typeorm";
 import {PostgresTypeOrmConfigurationProvider} from "./PostgresTypeOrmConfigurationProvider";
-
+import dotenv from "dotenv";
+dotenv.config();
 const PostgresMigrationsDataSource = new DataSource(
     PostgresTypeOrmConfigurationProvider.getTypeOrmConfig()
 );
--- src/database-sqlite/SqliteMigrationsHelper.ts
+++ src/database-sqlite/SqliteMigrationsHelper.ts
@@ -1,5 +1,7 @@
 import {DataSource} from "typeorm";
 import {SqliteTypeOrmConfigurationProvider} from "./SqliteTypeOrmConfigurationProvider";
+import dotenv from "dotenv";
+dotenv.config();
 
 const SqliteMigrationsDataSource = new DataSource(
     SqliteTypeOrmConfigurationProvider.getTypeOrmConfig()
