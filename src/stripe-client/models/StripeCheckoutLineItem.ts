36fba46d325fee0bbe9adc8b729e518b42948782
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-01-29 13:23:23 +1100

    fix: validators

--- src/stripe-client/models/StripeCheckoutLineItem.ts
+++ src/stripe-client/models/StripeCheckoutLineItem.ts
@@ -1,8 +1,12 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsInt, IsString} from "class-validator";
 
 export class StripeCheckoutLineItem {
     @ApiProperty()
+    @IsString()
     price!: string;
+
     @ApiProperty()
+    @IsInt()
     quantity!: number;
 }
