3b2e0bfb888b56123b8c900fbbeb997467e1157c
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-02-21 22:22:53 +1100

    fix: add admin routes

--- src/authz/DefaultAuthGuard.ts
+++ src/authz/DefaultAuthGuard.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {AuthGuard} from "@nestjs/passport";
 
 @Injectable()
+// eslint-disable-next-line @darraghor/nestjs-typed/injectable-should-be-provided
 export class DefaultAuthGuard extends AuthGuard("jwt") {
     constructor() {
         super();
--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -4,15 +4,17 @@ import {PersonController} from "./person.controller";
 import {Person} from "./entities/person.entity";
 import {LoggerModule} from "../logger/logger.module";
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {AuthzClientModule} from "../authzclient/authz-client.module";
 import {CoreModule} from "../root-app/core-app.module";
+import {AuthzModule} from "../authz";
+import {AuthzClientModule} from "../authzclient/authz-client.module";
 
 @Module({
     imports: [
         CoreModule,
         LoggerModule,
         TypeOrmModule.forFeature([Person]),
-        forwardRef(() => AuthzClientModule), // forwardRef is needed to avoid circular dependency (AuthzClientModule imports PersonModule)
+        AuthzClientModule,
+        forwardRef(() => AuthzModule), // forwardRef is needed to avoid circular dependency (AuthzClientModule imports PersonModule)
     ],
     controllers: [PersonController],
     providers: [PersonService],
