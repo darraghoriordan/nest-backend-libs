aa27e23b363e2ed567748040650066671a9ed13f
darraghoriordan-skedsocial <darragh@skedsocial.com>
2025-01-04 21:27:10 +1100

    feat: change to json

--- src/root-app/app.service.ts
+++ src/root-app/app.service.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Healthy and running";
+        return '{result: "Healthy and running"}';
     }
 }
